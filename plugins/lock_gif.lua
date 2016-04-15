445a877d91f2114a02e55b5fd89db6f60d843b50
TestMaster12 <aryapc320@yahoo.com>
2016-04-15 05:18:37 +0000

    Update lock_gif.lua

--- plugins/lock_gif.lua
+++ plugins/lock_gif.lua
@@ -10,22 +10,31 @@ local function run(msg, matches)
     local data = load_data(_config.moderation.data)
     if data[tostring(msg.to.id)] then
         if data[tostring(msg.to.id)]['settings'] then
-            if data[tostring(msg.to.id)]['settings']['lock_gif'] then
-                lock_gif = data[tostring(msg.to.id)]['settings']['lock_gif']
+            if data[tostring(msg.to.id)]['settings']['lock_mci'] then
+                lock_mci = data[tostring(msg.to.id)]['settings']['lock_mci']
             end
         end
     end
     local chat = get_receiver(msg)
     local user = "user#id"..msg.from.id
-    if lock_gif == "yes" then
-        send_large_msg(get_receiver(msg), "User @" .. msg.from.username .. " Gif & files share is not allowed here!")
-        chat_del_user(chat, user, ok_cb, true)
+    if lock_mci == "yes" then
+        send_large_msg(get_receiver(msg), "User > "..msg.from.username.." Hamrah Aval Ads Was Not Allowed Here!\nیوزر "..msg.from.username.." تبلیغات همراه اول ممنوع بود!")
+        channel_del_msg(channel, msg, ok_cb, true)
+        channel_del_user(channel, user, ok_cb, true)
     end
 end
  
 return {
   patterns = {
-  "%[(unsupported)%]"
+  "^Hamrah Aval",
+  "^hamrah Aval",
+  "^Hamrah aval",
+  "^hamrah aval",
+  "^همراه اول",
+  "^همراه اول  بات",
+  "^بات همراه اول",
+  "^sharzh",
+  "^شارژ همراه اول",
  },
   run = run
 }
